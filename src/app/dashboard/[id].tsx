f97728fa514b23126351abb10cabbfb15287c187
lionhard83 <carloleonardi83@gmail.com>
2023-08-10 15:28:47 +0200

    first commit

---
+++ src/app/dashboard/[id].tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>Page</p>
+    </main>
+  )
+}
---
+++ src/components/button.tsx
@@ -0,0 +1,5 @@
+import type { AppProps } from 'next/app'
+ 
+export default function Button({ Component, pageProps }: Partial<AppProps>) {
+  return <button >Bottone scemo</button>
+}
