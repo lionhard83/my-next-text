b5c8a776eb6184499093cd12d7e8dac3be7b91d6
lionhard83 <carloleonardi83@gmail.com>
2023-08-21 18:15:21 +0200

    add home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,65 @@
+type Character = {
+  id: number;
+  name: string;
+  status: 'Alive' | 'Dead' | 'unknown';
+  species: string;
+  type: string;
+  gender: 'Male' | 'Female' | 'Genderless' | 'unknown';
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+
+type Info = {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+};
+
+type RickAndMortyResponse = {
+  info: Info;
+  results: Character[];
+};
 
 
+export default async function Page() {
+  const res = await fetch('https://rickandmortyapi.com/api/character')
+  const data = await res.json() as RickAndMortyResponse;
 
-export default function Page() {
+  
   return (
-    <div style={{backgroundColor: 'red'}}>
-      <p>Page</p>
-      <a href="/">Home</a>
+    <div className="bg-white py-24 sm:py-32">
+    <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+      <div className="max-w-2xl">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper suspendisse.</p>
+      </div>
+      <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+        
+        {data.results.map((item, index) => (<li key={index}>
+          <div className="flex items-center gap-x-6">
+            <img className="h-16 w-16 rounded-full" src={item.image} alt="" />
+            <div>
+              <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{item.name}</h3>
+              <p className="text-sm font-semibold leading-6 text-indigo-600">{item.species}/{item.status}</p>
+            </div>
+          </div>
+        </li>)
+      
+        )}
+        
+      </ul>
     </div>
+</div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+      <Header />
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{children}</div>
+      </main>
+      </body>
     </html>
   )
 }
